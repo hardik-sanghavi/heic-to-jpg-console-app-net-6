0dae93dcd3a15f8002e9508cd96fee2b8f78984e
hardik-sanghavi <hardiksanghavi512@gmail.com>
2022-06-20 12:33:07 +0530

    Add project files.

---
+++ ImageConverter/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ImageMagick;
+
+//add the your source folder path in which has the .heic files
+string sourceFolderPath = @"E:\Photos\Maternity shoot-Helna";
+
+//add the your destination folder path where you add jpg files
+string destinationFolderPath = @"E:\Photos\Maternity shoot";
+
+string[] allfiles = Directory.GetFiles(sourceFolderPath, "*.heic", SearchOption.AllDirectories);
+
+foreach (var file in allfiles)
+{
+    FileInfo info = new FileInfo(file);
+    using (MagickImage image = new MagickImage(info.FullName))
+    {
+        // Save frame as jpg
+        image.Write(@$"{destinationFolderPath}\{info.Name}.jpg");
+    }
+}
